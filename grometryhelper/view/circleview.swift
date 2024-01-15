325107b0b065f35c67390b5068934f89c14dd217
Irving Meng <imeng@lcs.on.ca>
2024-01-15 12:00:19 -0500

    Organized project by adding Views and Model groups and a new view file named CircleView.

---
+++ grometryhelper/view/circleview.swift
@@ -0,0 +1,18 @@
+//
+//  circleview.swift
+//  grometryhelper
+//
+//  Created by 孟元森 on 2024-01-15.
+//
+
+import SwiftUI
+
+struct circleview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    circleview()
+}
