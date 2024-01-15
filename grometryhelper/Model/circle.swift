f9381a2fd439171b7695ce39bfd210c1269afce9
Irving Meng <imeng@lcs.on.ca>
2024-01-15 12:09:24 -0500

    Added a model file to describe a circle.

---
+++ grometryhelper/Model/circle.swift
@@ -0,0 +1,18 @@
+//
+//  circle.swift
+//  grometryhelper
+//
+//  Created by 孟元森 on 2024-01-15.
+//
+
+import SwiftUI
+
+struct circle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    circle()
+}
