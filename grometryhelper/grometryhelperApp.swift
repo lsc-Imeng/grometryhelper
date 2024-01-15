8a5ed0524d8a96d3b0c14bf7bfe96e0804c22d57
Irving Meng <imeng@lcs.on.ca>
2024-01-15 11:54:46 -0500

    Initial Commit

---
+++ grometryhelper/grometryhelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  grometryhelperApp.swift
+//  grometryhelper
+//
+//  Created by 孟元森 on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct grometryhelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
